e001f38b66e8b1a88fbf1adfb740650f8d05de99
Antonio Maffia <maffia@cs-sirio.local>
2020-10-16 00:21:52 +0200

    Build docker: TensorFlow-2.1.0

--- build_docker.sh
+++ build_docker.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-SW_FLD=`echo $TRAVIS_COMMIT_MESSAGE | sed 's@.*Build:\(.*\),.*@\1@'`
+SW_FLD=`echo $TRAVIS_COMMIT_MESSAGE | sed 's@.*Build docker:\(.*\),.*@\1@'`
+
+echo ${SW_FLD}
 SW_NAME=${SW_FLD%%-*}
+echo ${SW_NAME}
 SW_TAG=${SW_FLD#*-}
+echo ${SW_TAG}
 SW_INIT=${SW_NAME:0:1}
+echo ${SW_INIT}
 
 #docker build --tag ${DOCKER_USER}/${TRAVIS_REPO_SLUG##*/}: ./docker/${SW_INIT,}/${SW_NAME}/${SW_FLD}
 
@@ -11,4 +16,4 @@ echo "Building Docker container for: ${SW_FLD}"
 
 docker build --tag ${DOCKER_USER}/${SW_NAME}:${SW_TAG} ./docker/${SW_INIT,}/${SW_NAME}/${SW_TAG}
 
-export $IMAGE_TO_PUSH="${DOCKER_USER}/${SW_NAME}:${SW_TAG}"
+export IMAGE_TO_PUSH="${DOCKER_USER}/${SW_NAME}:${SW_TAG}"
